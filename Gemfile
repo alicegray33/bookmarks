777593b5eac0eb06220ea79fd8603931b9eb8f8a
Alice Gray <alicegray33@gmail.com>
2022-01-17 15:08:42 +0000

    Setting up all the files

---
+++ Gemfile
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '3.1.0'
+
+gem 'capybara'
+gem 'launchy'
+gem 'rspec'
+gem 'rubocop'
+gem 'selenium-webdriver'
+gem 'simplecov', require: false
+gem 'simplecov-console', require: false
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'webrick'
---
+++ app.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+# Another comment
+class BookmarkManager < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello World'
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
